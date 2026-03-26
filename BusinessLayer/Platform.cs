1c73adafa7591f7934c1c4430629208bee84f92d
danieldimitrova22-web <danieldimitrov_a22@schoolmath.eu>
2026-03-26 10:54:54 +0200

    Add presentation layer and reference it to the businnes layer

---
+++ BusinessLayer/Platform.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_layer
+{
+	public class Platform
+	{
+		[Key]
+
+		public int Id { get; private set; }
+		
+		[Required]
+		[MaxLength(50)]
+		
+		public string Name { get; private set; }
+		
+		[Required]
+		[MaxLength(40)]
+		public string Manufacturer { get; private set; }
+		
+		[Required]
+		public string Release_date { get; private set; }
+		
+		[Required]
+		public string Generation { get; private set; }
+		
+		
+		
+		
+		
+		public List<Game> Games { get; private set; }
+		
+		private Platform()
+		{
+			
+		}
+		
+		public Platform(string name, string manufacturer, string release_date, string generation) : this()
+		{
+			this.Name = name;
+			this.Manufacturer = manufacturer;
+			this.Release_date = release_date;
+			this.Generation = generation;
+		}
+	}
+}
+
+
